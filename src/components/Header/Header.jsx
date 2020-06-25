96d8a913bc83bc1c87228ebe472ef352db961c91
Padunk <abraham.agel@gmail.com>
2020-06-25 15:09:38 +0700

    Header - initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import styled, { keyframes } from "styled-components";
+
+function Header() {
+  return <div style={{height: '500vh'}}>Header</div>;
+}
+
+export { Header };
