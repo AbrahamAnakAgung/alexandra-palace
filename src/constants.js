afbf3776100a54cf7c943f0e9aeb2edbdc7641f6
Padunk <abraham.agel@gmail.com>
2020-06-25 15:09:22 +0700

    constant

---
+++ src/constants.js
@@ -0,0 +1,16 @@
+export const COLORS = {
+  transparent: "transparent",
+  blackPure: "black",
+  whitePure: "white",
+  whitePrimary: "hsl(0, 43%, 96%)",
+  grayPrimary: "hsl(30, 3%, 31%)",
+  primaryColor: "rgba(207, 0, 92, 0.9)",
+};
+
+export const TEXT_SIZE = {
+  large: "2.4rem",
+  normalLarge: "1.8rem",
+  normal: "1.6rem",
+  normalSmall: "1.4rem",
+  small: "1.2rem",
+};
