659fe2855cbc5239e0b6d48a11d5e8fcd5fcbee0
Padunk <abraham.agel@gmail.com>
2020-06-26 13:57:12 +0700

    adding color

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,18 @@ export const COLORS = {
   whitePrimary: "hsl(0, 43%, 96%)",
   grayPrimary: "hsl(30, 3%, 31%)",
   primaryColor: "rgba(207, 0, 92, 0.9)",
+  secondaryColor: "#AB7D5E",
+  red: "#EB3E38",
+  orange: "#FF722C",
+  green: "#53B975",
+  chocolateLight: "#BA5F1D",
+  greenYellow: "#B7D25A",
+  beige: "#DFB755",
+  blueSky: "#84A5CC",
+  blue: "#4D9BD8",
+  chocolateDark: "#663124",
+  pink: "#F7524D",
+  greenDark: "#89B062",
 };
 
 export const TEXT_SIZE = {
@@ -13,4 +25,11 @@ export const TEXT_SIZE = {
   normal: "1.6rem",
   normalSmall: "1.4rem",
   small: "1.2rem",
+};
+
+export const FONT_WEIGHT = {
+  light: 200,
+  normal: 400,
+  semiBold: 600,
+  bold: 800,
 };
