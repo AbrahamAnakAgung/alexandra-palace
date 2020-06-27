5edea044a26a7b5624b08c730d40cbed75615e63
Padunk <abraham.agel@gmail.com>
2020-06-27 12:30:27 +0700

    Adding Contents

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import { Layout } from "./components/Layout/Layout";
 import { Header } from "./components/Header/Header";
+import { SectionContents } from "./components/SectionContents/SectionContents";
+import { COLORS } from "./constants";
 
 function App() {
   return (
     <div>
       <Layout>
         <Switch>
-          <Route exact path='/Home'>
+          <Route exact path='/'>
             <MainWrapper>
               <Header />
+              <SectionContents />
             </MainWrapper>
           </Route>
         </Switch>
@@ -26,6 +29,7 @@ const MainWrapper = styled.div`
   height: 100%;
   overflow: visible;
   position: relative;
+  background: ${COLORS.whitePrimary};
 `;
 
 export default hot(App);
